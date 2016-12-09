70c2e775984b2f7dc1ef7f5740e4cb806d2e8d00
Paarth <parth.barot@botreeconsulting.com>
2016-12-09 11:38:30 +0530

    updating format to JSON

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
-post 'github_commits/create_comment', to: 'github_commits#create_comment'
+post 'github_commits/create_comment.json', to: 'github_commits#create_comment'
