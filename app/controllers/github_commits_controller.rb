21f801fd2c21615a2ca56680bcf74ffe0b6417d0
Paarth <parth.barot@botreeconsulting.com>
2016-12-31 16:22:15 +0530

    Rollback skipping auth filters

--- app/controllers/github_commits_controller.rb
+++ app/controllers/github_commits_controller.rb
@@ -1,6 +1,10 @@
 class GithubCommitsController < ApplicationController
+  
   unloadable
-  skip_before_action [:check_if_login_required, :verify_authenticity_token]
+   
+  skip_before_filter :check_if_login_required
+  skip_before_filter :verify_authenticity_token
+  
   GITHUB_URL = "https://github.com/"
 
   def create_comment
