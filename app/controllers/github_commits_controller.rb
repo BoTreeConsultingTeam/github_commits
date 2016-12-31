b890996825a480d230fc46395373548cd29ecaac
Paarth <parth.barot@botreeconsulting.com>
2016-12-31 17:18:27 +0530

    Putting before filter to respond with 500 on invalid requests

--- app/controllers/github_commits_controller.rb
+++ app/controllers/github_commits_controller.rb
@@ -5,38 +5,36 @@ class GithubCommitsController < ApplicationController
   skip_before_filter :check_if_login_required
   skip_before_filter :verify_authenticity_token
   
+  before_action :verify_signature?
+  
   GITHUB_URL = "https://github.com/"
 
   def create_comment
-    if verify_signature?
-      if params[:commits].present?
-        repository_name = params[:repository][:name]
-        branch = params[:ref].split("/").last
-        params[:commits].each do |last_commit|
-          message = last_commit[:message]
-          if message.present? && message.include?("#rm")
-            issue_id = message.partition("#rm").last.split(" ").first.to_i
-            issue = Issue.find_by(id: issue_id)
-          end
-          email = EmailAddress.find_by(address: last_commit[:author][:email])
-          user = email.present? ? email.user : User.where(admin: true).first
-
-          if last_commit.present? && issue.present?
-            author = last_commit[:author][:name]
-            notes = t('commit.message', author: author, 
-                                        branch: branch, 
-                                        message: message, 
-                                        commit_id: last_commit[:id],
-                                        commit_url: last_commit[:url])
-            issue.journals.create(journalized_id: issue_id, journalized_type: "Issue", user: user, notes: notes)
-          end
+    if params[:commits].present?
+      repository_name = params[:repository][:name]
+      branch = params[:ref].split("/").last
+      params[:commits].each do |last_commit|
+        message = last_commit[:message]
+        if message.present? && message.include?("#rm")
+          issue_id = message.partition("#rm").last.split(" ").first.to_i
+          issue = Issue.find_by(id: issue_id)
         end
-        nothing = true
-      else
-        nothing = "0"
+        email = EmailAddress.find_by(address: last_commit[:author][:email])
+        user = email.present? ? email.user : User.where(admin: true).first
+
+        if last_commit.present? && issue.present?
+          author = last_commit[:author][:name]
+          notes = t('commit.message', author: author, 
+                                      branch: branch, 
+                                      message: message, 
+                                      commit_id: last_commit[:id],
+                                      commit_url: last_commit[:url])
+          issue.journals.create(journalized_id: issue_id, journalized_type: "Issue", user: user, notes: notes)
+        end
       end
+      nothing = true
     else
-      nothing = false
+      nothing = "0"
     end
     respond_to do |format|
       format.json { render json: {nothing: nothing}, status: :ok }
@@ -47,6 +45,6 @@ class GithubCommitsController < ApplicationController
     request.body.rewind
     payload_body = request.body.read
     signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), Rails.configuration.secret_token, payload_body)
-    return Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
+    render status: 500 unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
   end
 end
