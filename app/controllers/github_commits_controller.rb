011f8ba435a4ea2a06a8cc414f708e0897b1502c
Paarth <parth.barot@botreeconsulting.com>
2016-12-31 16:44:39 +0530

    adding code for reverting with diff. code value in JSON

--- app/controllers/github_commits_controller.rb
+++ app/controllers/github_commits_controller.rb
@@ -8,33 +8,40 @@ class GithubCommitsController < ApplicationController
   GITHUB_URL = "https://github.com/"
 
   def create_comment
-    if params[:commits].present? && verify_signature?
-      repository_name = params[:repository][:name]
-      branch = params[:ref].split("/").last
-      params[:commits].each do |last_commit|
-        message = last_commit[:message]
-        if message.present? && message.include?("#rm")
-          issue_id = message.partition("#rm").last.split(" ").first.to_i
-          issue = Issue.find_by(id: issue_id)
+    if verify_signature?
+      if params[:commits].present?
+        repository_name = params[:repository][:name]
+        branch = params[:ref].split("/").last
+        params[:commits].each do |last_commit|
+          message = last_commit[:message]
+          if message.present? && message.include?("#rm")
+            issue_id = message.partition("#rm").last.split(" ").first.to_i
+            issue = Issue.find_by(id: issue_id)
+          end
+          email = EmailAddress.find_by(address: last_commit[:author][:email])
+          user = email.present? ? email.user : User.where(admin: true).first
+
+          if last_commit.present? && issue.present?
+            author = last_commit[:author][:name]
+            notes = t('commit.message', author: author, 
+                                        repository_name: repository_name, 
+                                        github_url: GITHUB_URL, 
+                                        branch: branch, 
+                                        message: message, 
+                                        commit_id: last_commit[:id],
+                                        commit_url: last_commit[:url])
+            issue.journals.create(journalized_id: issue_id, journalized_type: "Issue", user: user, notes: notes)
+          end
         end
-        email = EmailAddress.find_by(address: last_commit[:author][:email])
-        user = email.present? ? email.user : User.where(admin: true).first
-          
-        if last_commit.present? && issue.present?
-          author = last_commit[:author][:name]
-          notes = t('commit.message', author: author, 
-                                      repository_name: repository_name, 
-                                      github_url: GITHUB_URL, 
-                                      branch: branch, 
-                                      message: message, 
-                                      commit_id: last_commit[:id],
-                                      commit_url: last_commit[:url])
-          issue.journals.create(journalized_id: issue_id, journalized_type: "Issue", user: user, notes: notes)
-        end
+        nothing = true
+      else
+        nothing = "0"
       end
+    else
+      nothing = false
     end
     respond_to do |format|
-      format.json { render json: {nothing: true}, status: :ok }
+      format.json { render json: {nothing: nothing}, status: :ok }
     end
   end
 
