dab669bdd886f96e957a0d82f46eae812b93a0f5
Parth Barot <parth.barot@botreeconsulting.com>
2017-01-26 16:59:56 +0530

    Refactoring the code for controller.

--- app/controllers/github_commits_controller.rb
+++ app/controllers/github_commits_controller.rb
@@ -8,37 +8,56 @@ class GithubCommitsController < ApplicationController
   before_action :verify_signature?
   
   GITHUB_URL = "https://github.com/"
+  REDMINE_JOURNALIZED_TYPE = "Issue"
+  REDMINE_ISSUE_NUMBER_PREFIX = "#rm"
 
   def create_comment
+    
     if params[:commits].present?
+      
       repository_name = params[:repository][:name]
       branch = params[:ref].split("/").last
+      
       params[:commits].each do |last_commit|
         message = last_commit[:message]
-        if message.present? && message.include?("#rm")
-          issue_id = message.partition("#rm").last.split(" ").first.to_i
+        
+        if message.present? && message.include?(REDMINE_ISSUE_NUMBER_PREFIX)
+          issue_id = message.partition(REDMINE_ISSUE_NUMBER_PREFIX).last.split(" ").first.to_i
           issue = Issue.find_by(id: issue_id)
         end
-        email = EmailAddress.find_by(address: last_commit[:author][:email])
-        user = email.present? ? email.user : User.where(admin: true).first
 
         if last_commit.present? && issue.present?
+
+          email = EmailAddress.find_by(address: last_commit[:author][:email])
+          user = email.present? ? email.user : User.where(admin: true).first
+          
           author = last_commit[:author][:name]
+          
           notes = t('commit.message', author: author, 
                                       branch: branch, 
                                       message: message, 
                                       commit_id: last_commit[:id],
                                       commit_url: last_commit[:url])
-          issue.journals.create(journalized_id: issue_id, journalized_type: "Issue", user: user, notes: notes)
+          
+          issue.journals.create(journalized_id: issue_id, 
+                                journalized_type: REDMINE_JOURNALIZED_TYPE, 
+                                user: user, 
+                                notes: notes
+                               )
+          resp_json = {success: true}
+        else
+          resp_json = {success: false, error: t('lables.no_issue_found') }
         end
       end
-      nothing = true
+      
     else
-      nothing = "0"
+      resp_json = {success: false, error: t('lables.no_commit_data_found') }
     end
+
     respond_to do |format|
-      format.json { render json: {nothing: nothing}, status: :ok }
+      format.json { render json: resp_json, status: :ok }
     end
+
   end
 
   def verify_signature?
--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :github_commits do
-  name 'Github Commits plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  name 'Github commits plugin'
+  author 'Botree Technologies'
+  description 'Redmine plugin to push github updates on Redmine issues directly via Github webhook'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/BoTreeConsultingTeam/github_commits'
+  author_url 'https://github.com/BoTreeConsultingTeam'
 end
